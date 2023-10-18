0196043653369e29909117bf9a554642fabe797d
Frederik <frha618@student.sdu.dk>
2023-10-18 16:18:55 +0200

    added dfs in python

---
+++ graph/dfs/dfs.py
@@ -0,0 +1,21 @@
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            dfs(graph, neighbor, visited)
+
+if __name__ == "__main__":
+    graph = {
+        'A': ['B', 'C'],
+        'B': ['A', 'D', 'E'],
+        'C': ['A', 'F'],
+        'D': ['B'],
+        'E': ['B', 'F'],
+        'F': ['C', 'E']
+    }
+    start_node = 'A'
+    visited_nodes = set()
+    dfs(graph, start_node, visited_nodes)
+    print("Visited nodes:", visited_nodes)
